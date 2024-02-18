0e1c0d3ff7003bdab55d6474a83b265407e26129
Max Stupnikov <61547155+weirdkidsima@users.noreply.github.com>
2024-02-19 02:19:22 +0300

    Update 1.py

--- Math/Alehina/1.py
+++ Math/Alehina/1.py
@@ -8,27 +8,34 @@ U = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 # объединение множеств А и В
 union = A.union(B)
 print("Объединение А и В:", union)
+print("Битовая шкала объединения А и В:", bin(int(''.join(map(str, [i in union for i in U]))[::-1], 2)))
 
 # пересечение множеств
 intersection = A.intersection(B)
-print("пересечение множеств А и В:", intersection)
+print("Пересечение множеств А и В:", intersection)
+print("Битовая шкала пересечения А и В:", bin(int(''.join(map(str, [i in intersection for i in U]))[::-1], 2)))
 
-#  разность множеств А и В
+# разность множеств А и В
 difference_ab = A.difference(B)
-print("разность А и В:", difference_ab)
+print("Разность А и В:", difference_ab)
+print("Битовая шкала разности А и В:", bin(int(''.join(map(str, [i in difference_ab for i in U]))[::-1], 2)))
 
 # разность множеств В и А
 difference_ba = B.difference(A)
-print("разность В и А:", difference_ba)
+print("Разность В и А:", difference_ba)
+print("Битовая шкала разности В и А:", bin(int(''.join(map(str, [i in difference_ba for i in U]))[::-1], 2)))
 
 # дополнение A
 complement_a = U.difference(A)
-print("дополнение A:", complement_a)
+print("Дополнение A:", complement_a)
+print("Битовая шкала дополнения A:", bin(int(''.join(map(str, [i in complement_a for i in U]))[::-1], 2)))
 
 # дополнение B
 complement_b = U.difference(B)
-print("дополнение B:", complement_b)
+print("Дополнение B:", complement_b)
+print("Битовая шкала дополнения B:", bin(int(''.join(map(str, [i in complement_b for i in U]))[::-1], 2)))
 
 # симметрическая разность множеств А и В
 symetric_difference = A.symmetric_difference(B)
-print("симметрическая разность А и В:", symetric_difference)
+print("Симметрическая разность А и В:", symetric_difference)
+print("Битовая шкала симметрической разности А и В:", bin(int(''.join(map(str, [i in symetric_difference for i in U]))[::-1], 2)))
