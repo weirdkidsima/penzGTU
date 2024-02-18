1d8eaf370b80e298a4d3b4d2d5df1df0c215bc65
Max Stupnikov <61547155+weirdkidsima@users.noreply.github.com>
2024-02-19 02:26:14 +0300

    Update 1.py

--- Math/Alehina/1.py
+++ Math/Alehina/1.py
@@ -5,37 +5,45 @@ B = {3, 5, 6, 10}
 # универсальное множество U
 U = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
+# Функция для вывода битовой шкалы множества
+def print_bit_scale(s, name):
+    bit_scale = []
+    for i in range(1, max(s) + 1):
+        if i in s:
+            bit_scale.append('1')
+        else:
+            bit_scale.append('0')
+    print(f"Битовая шкала для множества {name}: {''.join(bit_scale)}")
+
+# Выводим битовые шкалы для всех множеств
+print_bit_scale(A, "A")
+print_bit_scale(B, "B")
+print_bit_scale(U, "U")
+
 # объединение множеств А и В
 union = A.union(B)
 print("Объединение А и В:", union)
-print("Битовая шкала объединения А и В:", bin(int(''.join(map(str, [i in union for i in U]))[::-1], 2)))
 
 # пересечение множеств
 intersection = A.intersection(B)
 print("Пересечение множеств А и В:", intersection)
-print("Битовая шкала пересечения А и В:", bin(int(''.join(map(str, [i in intersection for i in U]))[::-1], 2)))
 
 # разность множеств А и В
 difference_ab = A.difference(B)
 print("Разность А и В:", difference_ab)
-print("Битовая шкала разности А и В:", bin(int(''.join(map(str, [i in difference_ab for i in U]))[::-1], 2)))
 
 # разность множеств В и А
 difference_ba = B.difference(A)
 print("Разность В и А:", difference_ba)
-print("Битовая шкала разности В и А:", bin(int(''.join(map(str, [i in difference_ba for i in U]))[::-1], 2)))
 
 # дополнение A
 complement_a = U.difference(A)
 print("Дополнение A:", complement_a)
-print("Битовая шкала дополнения A:", bin(int(''.join(map(str, [i in complement_a for i in U]))[::-1], 2)))
 
 # дополнение B
 complement_b = U.difference(B)
 print("Дополнение B:", complement_b)
-print("Битовая шкала дополнения B:", bin(int(''.join(map(str, [i in complement_b for i in U]))[::-1], 2)))
 
 # симметрическая разность множеств А и В
 symetric_difference = A.symmetric_difference(B)
 print("Симметрическая разность А и В:", symetric_difference)
-print("Битовая шкала симметрической разности А и В:", bin(int(''.join(map(str, [i in symetric_difference for i in U]))[::-1], 2)))
